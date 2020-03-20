123611ee23d4d9670a2214e7af4ee7ab028d2821
Brendita... martinez <brendita_mtz@live.com.mx>
2020-03-20 17:26:44 -0600

    Chapter 4 Code Organization and Testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -135,7 +136,7 @@ class App extends Component { //Class component
   } 
 }
 
-{/*Functional Stateles Components */}
+/*Functional Stateles Components */
 const Search = ({ value, onChange, onSubmit, children})  =>
       <form onSubmit={onSubmit}>
         <input
@@ -180,7 +181,7 @@ const Table = ({ list, onDismiss }) =>
             )}
       </div>
 
-const Button = ({ onClick, className = '', children }) =>
+const Button = ({ onClick, className , children }) =>
       <button
         onClick = {onClick}
         className = {className}
@@ -189,4 +190,35 @@ const Button = ({ onClick, className = '', children }) =>
         {children}
       </button>
 
+Button.defaultProps = {
+  className: '',
+  };
+
+Search.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  value: PropTypes.string
+}
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+    objectID: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    url: PropTypes.string,
+    num_comments: PropTypes.number,
+    points: PropTypes.number,
+    })
+    ).isRequired,
+  onDismiss: PropTypes.func.isRequired,
+  };
+  
+
 export default App;
+
+export {Button,Search,Table}
